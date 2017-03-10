c1eab4328096435aa853a69de88c614b3ffe4afa
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2017-03-10 06:14:40 -0800

    fix sensiolabs feedback

--- DependencyInjection/SmileEzEEPhoneFormFieldExtension.php
+++ DependencyInjection/SmileEzEEPhoneFormFieldExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Yaml\Yaml;
 
 class SmileEzEEPhoneFormFieldExtension extends Extension implements PrependExtensionInterface
